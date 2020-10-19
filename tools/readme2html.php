567c4a9763cd7c16b1e07c0af670b5fd7ec1039d
JS Morisset <jsm@surniaulula.com>
2020-10-19 06:26:00 -0700

    update markdown parser

--- tools/readme2html.php
+++ tools/readme2html.php
@@ -10,13 +10,9 @@
 define( 'WP_DEBUG', true );
 define( 'WP_USE_THEMES', false );
 define( 'WP_PATH', '/var/www/wpadm/wordpress/' );
-define( 'LIB_PATH', '/home/jsmoriss/svn/github/surniaulula/surniaulula.github.io/trunk/' );
+define( 'LIB_PATH', WP_PATH . 'wp-content/plugins/wpsso/lib/ext/' );
 
 require_once WP_PATH . 'wp-load.php';
-
-require_once LIB_PATH . 'tools/markdown.php';
-
-require_once LIB_PATH . 'tools/parse-readme.php';
 
 $sections = array(
 	'description'  => 1,
@@ -34,45 +30,53 @@ if ( empty( $argv[1] ) ) {
 	exit( 1 );
 
 } else {
-	$readme_txt = $argv[1];
+
+	$readme_path = $argv[ 1 ];
 }
 
-if ( $fh = @fopen( $readme_txt, 'rb' ) ) {
+if ( $fh = @fopen( $readme_path, 'rb' ) ) {
 
-	$content = fread( $fh, filesize( $readme_txt ) );
+	$content = fread( $fh, filesize( $readme_path ) );
 
 	fclose( $fh );
 
 } else {
-	error_log( 'error opening ' . $readme_txt . ' for reading' );
+
+	error_log( 'error opening ' . $readme_path . ' for reading' );
 
 	exit( 1 );
 }
 
 if ( empty( $content ) ) {
 
-	error_log( 'no content read from ' . $readme_txt );
+	error_log( 'no content read from ' . $readme_path );
 
 	exit( 1 );
 }
 
-$parser = new SuextParseReadme();
-$info   = $parser->parse_readme_contents( $content );
+if ( ! class_exists( 'SuextParseReadme' ) ) {
 
-if ( empty( $info ) ) {
+	require_once LIB_PATH . 'parse-readme.php';
+}
+
+$parser =& SuextParseReadme::get_instance();
+
+$plugin_readme = $parser->parse_readme_contents( $content );
+
+if ( empty( $plugin_readme ) ) {
 
 	error_log( 'no info parsed from content' );
 
 	exit( 1 );
 }
 
-if ( strpos( $info[ 'title' ], ' | ' ) ) {
+if ( strpos( $plugin_readme[ 'title' ], ' | ' ) ) {
 
-	$title = preg_replace( '/^(.*) \| (.*)$/', '<h1>$1</h1><h3>$2</h3>', $info[ 'title' ] );
+	$title = preg_replace( '/^(.*) \| (.*)$/', '<h1>$1</h1><h3>$2</h3>', $plugin_readme[ 'title' ] );
 
 } else {
 
-	$title = '<h1>' . $info[ 'title' ] . '</h1>';
+	$title = '<h1>' . $plugin_readme[ 'title' ] . '</h1>';
 }
 
 echo $title . "\n\n";
@@ -83,60 +87,60 @@ $trth = '<tr><th align="right" valign="top" nowrap>';
 $thtd = '</th><td>';
 $tdtr = '</td></tr>' . "\n";
 
-if ( ! empty( $info[ 'plugin_name' ] ) ) {
+if ( ! empty( $plugin_readme[ 'plugin_name' ] ) ) {
 
-	echo $trth . 'Plugin Name' . $thtd . $info[ 'plugin_name' ] . $tdtr;
+	echo $trth . 'Plugin Name' . $thtd . $plugin_readme[ 'plugin_name' ] . $tdtr;
 }
 
-if ( ! empty( $info[ 'short_description' ] ) ) {
+if ( ! empty( $plugin_readme[ 'short_description' ] ) ) {
 
-	echo $trth . 'Summary' . $thtd . $info[ 'short_description' ] . $tdtr;
+	echo $trth . 'Summary' . $thtd . $plugin_readme[ 'short_description' ] . $tdtr;
 }
 
-if ( ! empty( $info[ 'stable_tag' ] ) ) {
+if ( ! empty( $plugin_readme[ 'stable_tag' ] ) ) {
 
-	echo $trth . 'Stable Version' . $thtd . $info[ 'stable_tag' ] . $tdtr;
+	echo $trth . 'Stable Version' . $thtd . $plugin_readme[ 'stable_tag' ] . $tdtr;
 }
 
-if ( ! empty( $info[ 'requires_php' ] ) ) {
+if ( ! empty( $plugin_readme[ 'requires_php' ] ) ) {
 
-	echo $trth . 'Requires PHP' . $thtd . $info[ 'requires_php' ] . ' or newer' . $tdtr;
+	echo $trth . 'Requires PHP' . $thtd . $plugin_readme[ 'requires_php' ] . ' or newer' . $tdtr;
 }
 
-if ( ! empty( $info[ 'requires_at_least' ] ) ) {
+if ( ! empty( $plugin_readme[ 'requires_at_least' ] ) ) {
 
-	echo $trth . 'Requires WordPress' . $thtd . $info[ 'requires_at_least' ] . ' or newer' . $tdtr;
+	echo $trth . 'Requires WordPress' . $thtd . $plugin_readme[ 'requires_at_least' ] . ' or newer' . $tdtr;
 }
 
-if ( ! empty( $info[ 'tested_up_to' ] ) ) {
+if ( ! empty( $plugin_readme[ 'tested_up_to' ] ) ) {
 
-	echo $trth . 'Tested Up To WordPress' . $thtd . $info[ 'tested_up_to' ] . $tdtr;
+	echo $trth . 'Tested Up To WordPress' . $thtd . $plugin_readme[ 'tested_up_to' ] . $tdtr;
 }
 
-if ( ! empty( $info[ 'wc_tested_up_to' ] ) ) {
+if ( ! empty( $plugin_readme[ 'wc_tested_up_to' ] ) ) {
 
-	echo $trth . 'Tested Up To WooCommerce' . $thtd . $info[ 'wc_tested_up_to' ] . $tdtr;
+	echo $trth . 'Tested Up To WooCommerce' . $thtd . $plugin_readme[ 'wc_tested_up_to' ] . $tdtr;
 }
 
-if ( ! empty( $info[ 'contributors' ] ) ) {
+if ( ! empty( $plugin_readme[ 'contributors' ] ) ) {
 
-	echo $trth . 'Contributors' . $thtd . ( implode( ', ', $info[ 'contributors' ] ) ) . $tdtr;
+	echo $trth . 'Contributors' . $thtd . ( implode( ', ', $plugin_readme[ 'contributors' ] ) ) . $tdtr;
 }
 
-if ( ! empty( $info[ 'donate_link' ] ) ) {
+if ( ! empty( $plugin_readme[ 'donate_link' ] ) ) {
 
-	echo $trth . 'WebSite URL</th><td><a href="' . $info[ 'donate_link' ] . '">' . $info[ 'donate_link' ] . '</a>' . $tdtr;
+	echo $trth . 'WebSite URL</th><td><a href="' . $plugin_readme[ 'donate_link' ] . '">' . $plugin_readme[ 'donate_link' ] . '</a>' . $tdtr;
 }
 
-if ( ! empty( $info[ 'license' ] ) ) {
+if ( ! empty( $plugin_readme[ 'license' ] ) ) {
 
-	echo $trth . 'License' . $thtd . ( empty( $info[ 'license_uri' ] ) ? $info[ 'license' ] :
-		'<a href="' . $info[ 'license_uri' ] . '">' . $info[ 'license' ] . '</a>' ) . $tdtr;
+	echo $trth . 'License' . $thtd . ( empty( $plugin_readme[ 'license_uri' ] ) ? $plugin_readme[ 'license' ] :
+		'<a href="' . $plugin_readme[ 'license_uri' ] . '">' . $plugin_readme[ 'license' ] . '</a>' ) . $tdtr;
 }
 
-if ( ! empty( $info[ 'tags' ] ) ) {
+if ( ! empty( $plugin_readme[ 'tags' ] ) ) {
 
-	echo $trth . 'Tags / Keywords' . $thtd . ( implode( ', ', $info[ 'tags' ] ) ) . $tdtr;
+	echo $trth . 'Tags / Keywords' . $thtd . ( implode( ', ', $plugin_readme[ 'tags' ] ) ) . $tdtr;
 }
 
 echo '</table>' . "\n\n";
@@ -145,37 +149,37 @@ if ( ! empty( $sections[ 'description' ] ) ) {
 
 	echo '<h2>Description</h2>' . "\n\n";
 
-	$info[ 'sections' ][ 'description' ] = preg_replace( 
+	$plugin_readme[ 'sections' ][ 'description' ] = preg_replace( 
 		array( '/`([^`]*)`/' ),
 		array( '<code>$1</code>' ),
-		$info[ 'sections' ][ 'description' ]
+		$plugin_readme[ 'sections' ][ 'description' ]
 	);
 
-	echo $info[ 'sections' ][ 'description' ] . "\n\n";
+	echo $plugin_readme[ 'sections' ][ 'description' ] . "\n\n";
 }
 
 if ( ! empty( $sections[ 'installation' ] ) ) {
 
 	echo '<h2>Installation</h2>' . "\n\n";
-	echo $info[ 'sections' ][ 'installation' ] . "\n\n";
+	echo $plugin_readme[ 'sections' ][ 'installation' ] . "\n\n";
 }
 
 if ( ! empty( $sections[ 'faq' ] ) ) {
 
 	echo '<h2>Frequently Asked Questions</h2>' . "\n\n";
-	echo $info[ 'sections' ][ 'faq' ] . "\n\n";
+	echo $plugin_readme[ 'sections' ][ 'faq' ] . "\n\n";
 }
 
 if ( ! empty( $sections[ 'screenshots' ] ) ) {
 
 	echo '<h2>Screenshots</h2>' . "\n\n";
 
-	if ( ! empty( $info[ 'screenshots' ] ) && ! empty( $info[ 'plugin_slug' ] ) ) {
+	if ( ! empty( $plugin_readme[ 'screenshots' ] ) && ! empty( $plugin_readme[ 'plugin_slug' ] ) ) {
 
-		foreach ( $info[ 'screenshots' ] as $num => $screenshot ) {
+		foreach ( $plugin_readme[ 'screenshots' ] as $num => $screenshot ) {
 
 			echo '<p align="center"><img align="center" src="https://surniaulula.github.io/' . 
-				$info[ 'plugin_slug' ] . '/assets/screenshot-' . sprintf( '%02d', $num + 1 ) . '.png"/><br/>' . "\n";
+				$plugin_readme[ 'plugin_slug' ] . '/assets/screenshot-' . sprintf( '%02d', $num + 1 ) . '.png"/><br/>' . "\n";
 
 			echo $screenshot . '</p>' . "\n\n";
 		}
@@ -185,16 +189,16 @@ if ( ! empty( $sections[ 'screenshots' ] ) ) {
 if ( ! empty( $sections[ 'changelog' ] ) ) {
 
 	echo '<h2>Changelog</h2>' . "\n\n";
-	echo $info[ 'sections' ][ 'changelog' ] . "\n\n";
+	echo $plugin_readme[ 'sections' ][ 'changelog' ] . "\n\n";
 }
 
 if ( ! empty( $sections[ 'notice' ] ) ) {
 
 	echo '<h2>Upgrade Notice</h2>' . "\n\n";
 
-	if ( ! empty( $info[ 'upgrade_notice' ] ) ) {
+	if ( ! empty( $plugin_readme[ 'upgrade_notice' ] ) ) {
 
-		foreach ( $info[ 'upgrade_notice' ] as $version => $notice ) {
+		foreach ( $plugin_readme[ 'upgrade_notice' ] as $version => $notice ) {
 
 			echo '<h4>' . $version . '</h4>' . "\n";
 			echo '<p>' . $notice . '</p>' . "\n\n";
